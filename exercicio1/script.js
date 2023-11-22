43e092515e5a04ee7237b0ca3eddcd45012490a9
Wumus <wumus.oficial@gmail.com>
2023-11-22 18:35:07 +0100

    Exercicíos completos 100%

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let conta = 0;
+let i = "S";
+
+function comerCoxinhas() {
+  while (i === "S") { //Enquanto i for igual a S execute a pergunta
+    i = prompt("Digite 'S' para comprar coxinhas e 'N' para deixar de comprar: ").toUpperCase();
+    conta = conta + 2.5;
+  }
+  console.log("Conta total: R$",conta);
+}
+
+comerCoxinhas();
