3c6694e6cc19932f252019f5e72d112d6ea0d232
Wumus <wumus.oficial@gmail.com>
2023-11-22 18:51:41 +0100

    exercícios corrigidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,12 +1,11 @@
-let conta = 0;
-let i = "S";
-
 function comerCoxinhas() {
-  while (i === "S") { //Enquanto i for igual a S execute a pergunta
-    i = prompt("Digite 'S' para comprar coxinhas e 'N' para deixar de comprar: ").toUpperCase();
-    conta = conta + 2.5;
-  }
-  console.log("Conta total: R$",conta);
+  let conta = 0;
+  let i = prompt("Digite 'S' para comprar coxinhas e 'N' para deixar de comprar: ").toUpperCase();
+while (i === "S") { //Enquanto i for igual a S execute a pergunta
+  i = prompt("Digite 'S' para comprar coxinhas e 'N' para deixar de comprar: ").toUpperCase();
+  conta = conta + 2.5;
+}
+console.log("Conta total: R$",conta);
 }
 
 comerCoxinhas();
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,8 @@
-const maioresPaises = ["","Rússia", "Canadá", "China", "EUA", "Brasil"] //Criando array
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"] //Criando array
 
 //i = 1, enquanto i for igual ou menor ao numero de indices, incremente +1
-for (let i = 1; i <= maioresPaises.length -1; i++) { 
+for (let i = 0; i <= maioresPaises.length -1; i++) { 
     
-    console.log(`Rank ${i}: ${maioresPaises[i]}`)
+    console.log(`Rank ${i+1}: ${maioresPaises[i]}`)
 }
 
